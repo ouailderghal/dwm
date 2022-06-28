d2172543bea20b41b92d836aa0f33eaf4695f72c
Ouail Derghal <ouailderghal@gmail.com>
2022-06-28 23:22:21 +0100

    Add Brave browser keybindings

--- config.def.h
+++ config.def.h
@@ -69,7 +69,7 @@ static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY,                       XK_b,      togglebar,      {0} },
+	/* { MODKEY,                       XK_b,      togglebar,      {0} }, */
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
 	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
@@ -102,7 +102,9 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
 
     /* My bindings */
+	{ MODKEY, XK_b, spawn, SHCMD("brave-browser") },
 	{ MODKEY|ShiftMask, XK_l, spawn, SHCMD("slock") },
+	{ MODKEY|ShiftMask, XK_b, spawn, SHCMD("brave-browser --incognito") },
 };
 
 /* button definitions */
